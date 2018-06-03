990b3e9e02181cb07e6c54021e911ca502b49f5e
Taylor Williams <taylor@thewilliams.net>
2018-06-03 23:36:37 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,10 +51,10 @@ class Owner
   end
 
   def sell_pets
-    
+
   end
 
   def list_pets
-    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cats(s)."
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
   end
 end
