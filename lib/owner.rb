0bfda38f5489d16e0d74e8afa98cd7e5123f153b
Taylor Williams <taylor@thewilliams.net>
2018-06-03 20:49:15 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,10 +3,11 @@ class Owner
   @@owners = []
   def initialize(name)
     @name = name
+    @@owners << self
   end
   # code goes here
   def self.all
-    @@owners
+    @@owners    
   end
 
   def self.reset_all
